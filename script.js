de7e90e6c2cec183daf9ad7c9236abc15dc3c5e9
Andres Samuel Ventura jr <andresventura@MacBook-Pro.home>
2021-05-21 17:52:21 -0700

    Pop up notification

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.querySelector('#button')
+const toasts = document.querySelector('#toasts')
+
+const message = [
+    'messageOne',
+    'messageTwo',
+    'messageThree',
+    'messageFour',
+]
+
+button.addEventListener('click', () => {
+    createNotification()
+})
+
+function createNotification() {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+
+    notif.innerText = getRandomMessage()
+
+    toasts.appendChild(notif)
+
+    setTimeout(() => {
+        notif.remove()
+    }, 3000)
+}
+
+function getRandomMessage() {
+    return message[Math.floor(Math.random() * message.length)]
+}
